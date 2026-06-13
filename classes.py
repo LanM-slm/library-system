9b10bd941d7fcee3509b7087db13179af920ad73
Lana <merkosyanlana@gmail.com>
2026-06-13 14:14:26 +0400

    5

--- classes.py
+++ classes.py
@@ -13,7 +13,7 @@ class WriteBook:
     def write(books):
         try:
             with open('books.json', 'w', encoding='utf-8') as f:
-                json.dump(books, f, indent=4)
+                json.dump(books, f, ensure_ascii=False, indent=4)
         except FileNotFoundError:
             print('Book file does not exist!')
             exit()
@@ -48,7 +48,7 @@ class WriteUser:
     def write(user):
         try:
             with open('user.json', 'w', encoding='utf-8') as f:
-                json.dump(user, f, indent=4)
+                json.dump(user, f, ensure_ascii=False, indent=4)
         except FileNotFoundError:
             print('Users file does not exist!')
             exit()
